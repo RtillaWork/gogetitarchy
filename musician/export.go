388abcc8bb5a8bb078f80874b4d755af0cdf27d6
NNNE <39413995+RtillaWork@users.noreply.github.com>
2022-04-05 12:15:10 -0600

    init clean-code

--- musician/export.go
+++ musician/export.go
@@ -76,29 +76,6 @@ func ExportCsv(musicians MusiciansMap, filename string) {
 	log.Printf("\n\n\n SIZE of musicians: %d\n\n", counter)
 }
 
-//func ExportMusiciansDb(db MusiciansDb, filename string) {
-//	var outfile *os.File
-//	if filename == "" || !strings.HasSuffix(filename, ".csv") {
-//		outfile = os.Stdout
-//	} else if h, err := os.Open("OUT_MUSICIANS_DATADICT" + filename); err != nil {
-//		log.Printf("Error opening file: %s \n%v\n", outfile, err)
-//		outfile = os.Stdout
-//	} else {
-//		outfile = h
-//	}
-//	counter := 1
-//	for _, m := range db {
-//		//log.Printf("{KEY: %s ,,,, VALUE: {FIRST: %s  L: %s   MIDDLE:  %s   NOTES: %s  }", k, m.FName, m.LName, m.MName, m.Notes)
-//		//log.Println(m.ToCsv())
-//		if outfile == os.Stdout {
-//			fmt.Fprintf(outfile, "\n===================")
-//		}
-//		fmt.Fprintf(outfile, "\n%d; %s\n", counter, m.ToCsv())
-//		counter++
-//	}
-//	log.Printf("\n\n\n SIZE of musicians: %d\n\n", counter)
-//}
-
 func ExportDataDict(dict DataDict, filename string) {
 	var outfile *os.File
 	if filename == "" || !strings.HasSuffix(filename, ".csv") {
